8a6331c945e7902695467f49383318b28c72c484
Crzax <115697417+Crzax@users.noreply.github.com>
2024-08-21 08:49:00 +0800

    delete rust lsp.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,9 +2,9 @@
 require('mason').setup({
     ui = {
         icons = {
-            package_installed = "✓",   -- Icon to display for installed packages
-            package_pending = "➜",     -- Icon to display for packages that are being installed
-            package_uninstalled = "✗"  -- Icon to display for packages that are not installed
+            package_installed = "✓", -- Icon to display for installed packages
+            package_pending = "➜", -- Icon to display for packages that are being installed
+            package_uninstalled = "✗" -- Icon to display for packages that are not installed
         }
     }
 })
@@ -12,7 +12,7 @@ require('mason').setup({
 -- Setup Mason LSPconfig, which integrates Mason with Neovim's built-in LSP client configuration.
 require('mason-lspconfig').setup({
     -- A list of LSP servers to automatically install if they're not already installed.
-    ensure_installed = { 'pylsp', 'lua_ls', 'rust_analyzer', 'texlab' },
+    ensure_installed = { 'pylsp', 'lua_ls', 'texlab' },
 })
 
 -- Set different settings for different languages' LSP
@@ -26,10 +26,10 @@ local lspconfig = require('lspconfig')
 -- This function will be called when an LSP server attaches to a buffer, allowing us to set up key mappings and other buffer-local settings.
 -- Documentation: `:help vim.diagnostic.*` for more information on the diagnostic functions used below.
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)      -- Show diagnostics in a floating window
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)              -- Jump to the previous diagnostic
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)              -- Jump to the next diagnostic
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)       -- Add buffer diagnostics to the location list
+vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts) -- Show diagnostics in a floating window
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)        -- Jump to the previous diagnostic
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)        -- Jump to the next diagnostic
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts) -- Add buffer diagnostics to the location list
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer.
@@ -39,22 +39,22 @@ local on_attach = function(client, bufnr)
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions.
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)          -- Go to declaration
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)           -- Go to definition
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)                 -- Hover to show information
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)       -- Go to implementation
-    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)    -- Show signature help
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)  -- Add workspace folder
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)  -- Remove workspace folder
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)                    -- Go to declaration
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)                     -- Go to definition
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)                           -- Hover to show information
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)                 -- Go to implementation
+    vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)              -- Show signature help
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)    -- Add workspace folder
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts) -- Remove workspace folder
     vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))          -- List workspace folders
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))                   -- List workspace folders
     end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)  -- Go to type definition
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)          -- Rename symbol
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)     -- Show code actions
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)             -- List all references
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)          -- Go to type definition
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)                  -- Rename symbol
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)             -- Show code actions
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)                     -- List all references
     vim.keymap.set("n", "<space>f", function()
-        vim.lsp.buf.format({ async = true })                               -- Format the current buffer
+        vim.lsp.buf.format({ async = true })                                       -- Format the current buffer
     end, bufopts)
 end
 
@@ -65,7 +65,7 @@ end
 
 -- Python LSP Server
 lspconfig.pylsp.setup({
-	on_attach = on_attach,
+    on_attach = on_attach,
 })
 
 -- LaTeX LSP Server
@@ -75,25 +75,25 @@ lspconfig.texlab.setup({
 
 -- Lua LSP Server
 lspconfig.lua_ls.setup({
-	on_attach = on_attach,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
